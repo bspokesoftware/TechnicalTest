9e3269667de500c4ee5e0bd9a849ae74453aeed0
keagan <keagan8simpson@gmail.com>
2025-08-21 18:35:22 +0200

    Implemention the scenario IsFrozen.

--- TechnicalTest.Data/BankAccount.cs
+++ TechnicalTest.Data/BankAccount.cs
@@ -4,7 +4,7 @@ public class BankAccount
 {
     public int Id { get; set; }
     public required string AccountNumber { get; set; }
-    
+    public bool IsFrozen { get; set; }
     public int CustomerId { get; set; }
     public Customer? Customer { get; set; }
 }

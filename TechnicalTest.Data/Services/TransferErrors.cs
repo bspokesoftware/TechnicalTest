cdb9d2c82bbd2efc747fb1024c59617b5c0b0634
keagan <keagan8simpson@gmail.com>
2025-08-21 21:26:09 +0200

    Going with route of declaring errors as enums to show I know how to use enums.

---
+++ TechnicalTest.Data/Services/TransferErrors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalTest.Data.Services
+{
+    public enum TransferErrorCode
+    {
+        NotFound,
+        ValidationFailed,
+        BusinessRuleViolation
+    }
+    public class TransferErrors : Exception
+    {
+    }
+}
---
+++ TechnicalTest.Data/Services/TransferService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalTest.Data.Services
+{
+    public sealed class TransferService : ITransferService
+    {
+        private readonly ApplicationContext _context;
+
+        public TransferService(ApplicationContext context) => _context = context;
+
+        public async Task<Transfer> CreateAsync(
+            int formAccountId,
+            int toAccountId,
+            decimal amount,
+            string? reference,
+            CancellationToken cancellationToken = default
+            )
+        {
+
+        }
+    }
+}

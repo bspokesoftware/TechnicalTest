aa54e31df5d48b2459ca729e797c607ece3c2612
keagan <keagan8simpson@gmail.com>
2025-08-21 19:12:28 +0200

    Added method OnModelCreating. This is due to the database being sensitive. There are also differences from a normal SQL database and a SQLite database. I implemented this method to make up for the differences and to ensure the database works as it should when using SQLite.

--- TechnicalTest.Data/ApplicationContext.cs
+++ TechnicalTest.Data/ApplicationContext.cs
@@ -12,4 +12,43 @@ public class ApplicationContext : DbContext
 
     public DbSet<Customer> Customers { get; set; } = null!;
     public DbSet<BankAccount> BankAccounts { get; set; } = null!;
+    public DbSet<Transfer> Transfers { get; set; } = null!;
+
+    // Adding rules to database due to this being a banking app
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //Customer
+        modelBuilder.Entity<Customer>(e =>
+        {
+            e.Property(c => c.Name).IsRequired();
+            e.Property(c => c.DailyTransferLimit).HasPrecision(18, 2);
+        });
+
+        //Bank Account
+        modelBuilder.Entity<BankAccount>(e =>
+        {
+            e.Property(a => a.AccountNumber).IsRequired();
+            e.HasIndex(a => a.AccountNumber).IsUnique();
+            e.HasMany(a => a.TransfersOut).WithOne(t => t.FromAccount)
+                .HasForeignKey(t => t.FromAccountId).OnDelete(DeleteBehavior.Restrict);
+
+            e.HasMany(a => a.TransfersIn).WithOne(t => t.ToAccount)
+                .HasForeignKey(t => t.ToAccountId).OnDelete(DeleteBehavior.Restrict);
+        });
+
+        //Transer
+        modelBuilder.Entity<Transfer>(e =>
+        {
+            e.Property(t => t.Amount).HasPrecision(18, 2);
+            e.Property(t => t.Reference).HasMaxLength(240);
+            e.HasIndex(t => t.CreatedAtUTC);
+            e.ToTable(tb =>
+            {
+                tb.HasCheckConstraint("CK_Transfer_Amount_Positive", "[Amount] > 0");
+            });
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
--- TechnicalTest.Data/Customer.cs
+++ TechnicalTest.Data/Customer.cs
@@ -4,7 +4,7 @@ public class Customer
 {
     public int Id { get; set; }
     public required string Name { get; set; }
-    public required DateTime DateOfBirth { get; set; }
+    public DateTime DateOfBirth { get; set; }
     public decimal DailyTransferLimit { get; set; }
 
     // Collection of bank accounts owned by a customer

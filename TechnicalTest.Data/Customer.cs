be1c90651d6792e28b5b23a177e3a299744e06e8
keagan <keagan8simpson@gmail.com>
2025-08-21 18:33:38 +0200

    Adding the changes that were mentioned in the scenario.

--- TechnicalTest.Data/Customer.cs
+++ TechnicalTest.Data/Customer.cs
@@ -4,5 +4,9 @@ public class Customer
 {
     public int Id { get; set; }
     public required string Name { get; set; }
+    public required DateTime DateOfBirth { get; set; }
+    public decimal DailyTransferLimit { get; set; }
+
+    // Collection of bank accounts owned by a customer
     public ICollection<BankAccount> BankAccounts { get; set; } = new HashSet<BankAccount>();
 }

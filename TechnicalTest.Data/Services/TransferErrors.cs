cda4465976509e0435fb2cf2d22bc5bf97ee177b
keagan <keagan8simpson@gmail.com>
2025-08-21 21:31:05 +0200

    Full implementation of the enum errors for transactions

--- TechnicalTest.Data/Services/TransferErrors.cs
+++ TechnicalTest.Data/Services/TransferErrors.cs
@@ -12,7 +12,15 @@ namespace TechnicalTest.Data.Services
         ValidationFailed,
         BusinessRuleViolation
     }
-    public class TransferErrors : Exception
+    public class TransferException : Exception
     {
+        public TransferErrorCode ErrorCode { get; }
+
+        public TransferException(TransferErrorCode errorCode, string message) : base(message) => ErrorCode = errorCode;
+
+        public static TransferException NotFound(string message) => new TransferException(TransferErrorCode.NotFound, message);
+        public static TransferException ValidationFailed(string message) => new TransferException(TransferErrorCode.ValidationFailed, message);
+        public static TransferException BusinessRuleViolation(string message) => new TransferException(TransferErrorCode.BusinessRuleViolation, message);
+
     }
 }
